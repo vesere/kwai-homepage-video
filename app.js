cec2df7763e934f5edc9f4d9e77798da308c6f91
whriten <9117143+whriten@user.noreply.gitee.com>
2022-12-28 11:12:29 +0800

    文件昵称校验

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ let filePath = 'http://127.0.0.1:5501/ceshi.mp4'
 
 let cookie = 'kpf=PC_WEB; kpn=KUAISHOU_VISION; clientid=3; client_key=65890b29; did=web_53be2bbf27357e691928269e84f402c3; didv=1666430874511; kuaishou.web.cp.api_st=ChZrdWFpc2hvdS53ZWIuY3AuYXBpLnN0EqABO6y0fz9ujXCqE36twtIqinXZhvFtVewFs00_qLJiP819c7QI_lU39DKFKnKBWg_0KCWnuGk1B02KMk8A-v16dxA4DDOGNPDTRedxS0nU-vWVLv4hfDelvhX41XZP5Ck7O9wX5aoiOuX85oDh3HTe118kVsqcL-bD88QoxZH5l3uLAxMbaBl8yLPmQwrwODvQlNLs9YdC0Nqa2zSiWvH7eRoSP67bVFjAYrLDKkZ-Dc4stgeaIiDH10Kfe3fjdVgdStIvrsoG8j8KgAvT0kS5VPtPdYXLdCgFMAE; kuaishou.web.cp.api_ph=065c68078697fd4f19f9a7d49f82e021e3d0; userId=546940635; kuaishou.server.web_st=ChZrdWFpc2hvdS5zZXJ2ZXIud2ViLnN0EqAB7BRHJZ83nSMrfwKulRzWkFy-lVaqHQFGeCAh2P_SK0SuWG1jkrdCuNOkl43Da0WwRexGPijV1eXX644A3pkDSUzq9E0EQ9h1BUC3vrGHl1B72UN1I2XBGEifmt5GH4gpvfB7S0r06qirxUihAsXB7fZonP_tfoCxyQ1vUUHLXtMnNw-ISsZXMRD7i70gb6xVrcc7ZadXygzJWB7dD-JJmhoStEyT9S95saEmiR8Dg-bb1DKRIiAH0aMRDJdok013EEiBZ5m8uuxjv6QCdB5VZ5tBQipa-igFMAE; kuaishou.server.web_ph=338692b251c65f0a45e6cc05526da454466e'
 
-let userId = "3xi8vrehcwr2ece"
+let userId = "3xwpfrup2mpach9"
 
-let pcursor = "1.671031332916E12"
+let pcursor = ""
 
 // 当没有视频时候返回的参数 pcursor
 // no_more
 
-// let  variables = `{"userId": "${userId}","pcursor":"${pcursor}","page": "profile"}`
 let  variables = `{"userId": "${userId}","pcursor":"${pcursor}","page": "profile"}`
 // console.log(variables);
 
@@ -36,9 +35,11 @@ function dowload(){
         pcursor = val.data.data.visionProfilePhotoList.pcursor
         console.log('下一页请求参数',pcursor);
         result.map((item,index)=>{
-            // 全部执行  下载函数
+
+            // 判断文件名是否符合文件命名规范
+            let fileName = item.photo.caption.replace(/[\\\\/:*?\"<>|]/g,'')
             setTimeout(()=>{
-                wrmp4(item.photo.photoUrl,item.photo.caption)
+                wrmp4(item.photo.photoUrl,fileName)
             },Math.random()*10000)
             
             // 判断是否为最后一项
@@ -59,8 +60,8 @@ function wrmp4(url,title){
     let buf = val.data
     let file = Buffer.from(buf)
 
-        // 一异步api不能使用try catch捕获错误 要在回调
-        fs.writeFile(`./视频/${title}.mp4`,file,(err)=>{
+        // 异步api不能使用try catch捕获错误 要在回调
+        fs.writeFile(`./小嫣/${title}.mp4`,file,(err)=>{
             count++
             console.log('写入完成--第'+count+'部');
             if(err){
@@ -68,11 +69,14 @@ function wrmp4(url,title){
             }
             // 判断当count等于返回结果长度时进行请求下一页 并改变参数
             if(count == resultLength){
-                // setTimeout(()=>{
-                //     variables = `{"userId":"${userId}","pcursor":"${pcursor}","page": "profile"}`
-                //     dowload()
-                // },Math.random()*10000)
+                variables = `{"userId":"${userId}","pcursor":"${pcursor}","page": "profile"}`
                 console.log('准备下次请求执行dowm函数');
+                console.log('请求参数为', variables);
+                if(pcursor=='no_more'){
+                    return console.log('----------主页视频已经采集完成---------');
+                }
+                dowload()
+                console.log('正在执行本次请求---');
             }
         })
 })
